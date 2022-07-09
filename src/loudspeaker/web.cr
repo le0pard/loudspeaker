6e8d48575b174b23b355901763e61b90ca54f6d7
Alexey Vasiliev <leopard.not.a@gmail.com>
2022-07-10 01:05:25 +0300

    add session

--- src/loudspeaker/web.cr
+++ src/loudspeaker/web.cr
@@ -1,4 +1,5 @@
 require "kemal"
+require "kemal-session"
 
 # Matches GET "http://host:port/"
 get "/" do
@@ -11,5 +12,13 @@ ws "/socket" do |socket|
   socket.send "Hello from Kemal!"
 end
 
-Kemal.config.serve_static = false
+Kemal.config do |config|
+  config.serve_static = false
+end
+Kemal::Session.config do |config|
+  config.cookie_name = "__loudspeaker_session"
+  config.secret = "a_secret"
+  # config.engine = Session::RedisEngine.new(host: "localhost", port: 6379, key_prefix: "session:")
+  # config.timeout = Time::Span.new(1, 0, 0)
+end
 Kemal.run(3000)
