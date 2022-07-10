a28e043eefef8df5b7c19f636ce208e4f0c6b3d2
Alexey Vasiliev <leopard.not.a@gmail.com>
2022-07-10 22:44:15 +0300

    add config

---
+++ src/loudspeaker/web/handlers/log_handler.cr
@@ -0,0 +1,35 @@
+require "kemal"
+
+module Loudspeaker
+  module Web
+    class LogHandler < Kemal::BaseLogHandler
+      Log = ::Log.for(self)
+
+      def call(context : HTTP::Server::Context)
+        elapsed_time = Time.measure { call_next context }
+        elapsed_text = elapsed_text(elapsed_time)
+        Log.dexter.debug do
+          {
+            status:  context.response.status_code,
+            method:  context.request.method,
+            path:    context.request.resource,
+            time:    elapsed_text,
+            message: "Response from web",
+          }
+        end
+        context
+      end
+
+      def write(message : String)
+        Log.debug { message }
+      end
+
+      private def elapsed_text(elapsed : Time::Span)
+        millis = elapsed.total_milliseconds
+        return "#{millis.round(2)}ms" if millis >= 1
+
+        "#{(millis * 1000).round(2)}µs"
+      end
+    end
+  end
+end
