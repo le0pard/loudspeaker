c2a432f55f454991a1a9681cc076c043f8173ab4
Alexey Vasiliev <leopard.not.a@gmail.com>
2022-07-12 22:26:10 +0300

    testing linking

--- src/loudspeaker/libav/avcodec.cr
+++ src/loudspeaker/libav/avcodec.cr
@@ -2,18 +2,18 @@ module Loudspeaker
   module LibAV
     @[Link("avcodec")]
     lib AVCodecBinding
-      fun version = avcodec_version : UInt32
+      fun avcodec_version : UInt32
     end
 
     module AVCodec
       extend self
 
       def version
-        v = AVCodecBinding.version
+        v = AVCodecBinding.avcodec_version
         "#{v >> 16}.#{(v & 0x00FF00) >> 8}.#{v & 0xFF}"
       end
     end
   end
 end
 
-# puts Loudspeaker::LibAV::AVCodec.version
+puts Loudspeaker::LibAV::AVCodec.version
